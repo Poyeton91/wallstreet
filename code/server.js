d77ee7c5e35a60a87332a2864a49bdd144640a32
spoyeto <sacha.poyeton@universite-paris-saclay.fr>
2026-03-20 15:01:48 +0100

    Gestion du port pour hebergeur public

--- code/server.js
+++ code/server.js
@@ -9,7 +9,7 @@ const io = new Server(server);
 
 app.use(express.static(path.join(__dirname, "public")));
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // ===== ÉTAT DU JEU =====
 let gameState = {
